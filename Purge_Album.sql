434983965bef180e92574933c28797f62040063a
ungvar71 <93205409+ungvar71@users.noreply.github.com>
2021-10-26 19:07:29 +0200

    Delete Purge_Album.sql

--- Purge_Album.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM album_artist WHERE album_artist.idAlbum  NOT IN (SELECT album.idAlbum  FROM album WHERE album.idAlbum IS NOT NULL)
-ou 
-SELECT * FROM album_artist WHERE album_artist.idAlbum  NOT IN (SELECT album.idAlbum  FROM album WHERE album.idAlbum IS NOT NULL)
