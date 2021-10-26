c166842814a3575df685ff72bc19a32413f27dab
ungvar71 <93205409+ungvar71@users.noreply.github.com>
2021-10-26 16:12:50 +0200

    Create New_index.sql

---
+++ New_index.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idAlbum_idx ON song(idAlbum);
+CREATE INDEX idArtist_idx ON song(idArtist);
+CREATE INDEX idArtist_idx ON album(idArtist);
+CREATE INDEX idArtist_idx ON exartistsong(idArtist);
+CREATE INDEX idArtist_idx ON exartistalbum(idArtist);
